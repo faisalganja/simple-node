829179eaae3c6f6d506b5b79c7b6ce0051a63765
jl <jl@localhost.localdomain>
2025-03-09 10:42:09 +0000

    adjusted main server file

--- server.js
+++ server.js
@@ -10,3 +10,4 @@ const server = http.createServer((req, res) => {
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
+}); // <-- Add this missing closing bracket
